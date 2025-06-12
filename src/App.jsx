aa806b6e1893e3c8e939a225ad14d7cd817257d7
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-12 16:10:10 +0300

    made app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/order",
+    element: <Order />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
+
 function App() {
-  return <h1>hello vite</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
