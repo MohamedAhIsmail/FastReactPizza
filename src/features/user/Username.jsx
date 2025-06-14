60b35465ee30735068f6af1bc5bde41c5a1790fc
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-14 10:50:02 +0300

    setting up redux and create useSlice + handel submit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Mohamed</div>;
+  const userName = useSelector((store) => store.user.userName);
+  console.log(userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default Username;
